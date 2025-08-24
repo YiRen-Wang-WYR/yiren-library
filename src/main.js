2839a15392133406423f4a225469788a62c531ab
YiRen <ywan1154@student.monash.edu>
2025-08-24 22:32:21 +1000

    Add PrimeVue components and styles to enhance UI

--- src/main.js
+++ src/main.js
@@ -1,7 +1,25 @@
-// import './assets/main.css'
-// import './style.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
 import { createApp } from 'vue'
 import App from './App.vue'
+// import bootstrap css
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import './style.css
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura';
 
-createApp(App).mount('#app')
+
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import ColumnGroup from 'primevue/columngroup';   // optional
+import Row from 'primevue/row';                   // optional
+const app = createApp(App);
+app.use(PrimeVue, {
+    theme: {
+        preset:Aura
+    }
+})
+app.component('DataTable', DataTable);
+app.component('Column', Column);
+app.component('ColumnGroup', ColumnGroup);
+app.component('Row', Row);
+
+app.mount('#app');
